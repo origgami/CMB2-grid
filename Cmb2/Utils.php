2574ba47b810ff69757670bbb5f046272d4013bb
Eric Klingensmith <eric@threadgroup.com>
2019-02-06 11:53:39 -0500

    Fixed group field init function and added group field append function

--- Cmb2/Utils.php
+++ Cmb2/Utils.php
@@ -15,11 +15,24 @@ if ( ! class_exists( '\Cmb2Grid\Cmb2\Utils' ) ) {
 			return $field;
 		}
 
-		public static function initializeGroupFieldArg( \CMB2_Field $field, $arg ) {
-			if ( ! isset( $field[ $arg ] ) ) {
-				$field[ $arg ] = '';
-			}
-			return $field;
+		public static function &initializeGroupFieldArg( \CMB2_Field $group_field, $fieldID, $arg ) {
+            if ( isset( $group_field->args[ 'fields' ][ $fieldID ] ) ) {
+                $actual_field = &$group_field->args[ 'fields' ][ $fieldID ];
+
+                // Make sure we have SOME value in before_row
+                if( !isset( $actual_field[$arg] ) ) $actual_field[$arg] = '';
+
+                // Return the actual field
+                return $actual_field;
+            }
+		}
+
+        public static function appendGroupFieldArg( \CMB2_Field $group_field, $fieldID, $arg, $appendString ) {
+		    // Get the actual field by reference and initialize it if needed
+		    $actual_field = &self::initializeGroupFieldArg( $group_field, $fieldID, $arg );
+
+            // Append the grid row class before row
+            $actual_field[$arg] .= $appendString;
 		}
 	}
 }
